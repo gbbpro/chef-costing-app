95015bcd74b35a917c4acaf19d24beb002f67b3f
gbbpro <78455428+gbbpro@users.noreply.github.com>
2026-04-23 22:12:12 -0600

    fix: prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,15 @@
-import type { PrismaClient as PrismaClientType } from "@prisma/client/default"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { PrismaClient } = require("@prisma/client")
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClientType }
+const globalForPrisma = globalThis as unknown as { prisma: any }
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 })
 
-const prisma: PrismaClientType =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter })
+const prisma: any = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
